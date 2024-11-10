b51b89cc4ce056351e82885149c33b190ae28acb
Tucker Gordon <tucker.gordon18@gmail.com>
2024-11-10 13:51:51 -0500

    Setup CORS

--- python/api/index.py
+++ python/api/index.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 
-# origins = [
-#     "http://localhost.tiangolo.com",
-#     "https://localhost.tiangolo.com",
-#     "http://localhost",
-#     "http://localhost:8080",
-# ]
+# The production and localhost URLs
+origins = [
+    "https://fantasy-fb.vercel.app",
+    "https://fantasy-dashboard-git-main-tuckergordons-projects.vercel.app",
+    "https://fantasy-dashboard-tuckergordons-projects.vercel.app",
+    "http://localhost",
+    "http://localhost:5173",
+    "http://localhost:4173"
+]
 
-origins = ["*"]
+# Regex to capture other URLs (e.g. for branches)
+# ^https://fantasy-dashboard: Matches the fixed part at the start of the URL.
+# (-[a-zA-Z0-9]+)*: Matches any hyphen followed by alphanumeric characters (0 or more times).
+# -tuckergordons-projects\.vercel\.app: Matches the fixed ending of the URL.
+# /?$: Allows for an optional trailing slash and asserts the end of the URL.
+origins_regex = "^https://fantasy-dashboard(-[a-zA-Z0-9]+)*-tuckergordons-projects\.vercel\.app/?$"
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=origins_regex,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    # For now only allowing GET requests
+    allow_methods=["GET"]
 )
 
 @app.get("/api/")
